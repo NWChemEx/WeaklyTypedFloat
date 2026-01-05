4283c84e21df254fe023512edb11bd48ca2acf9c
ryanmrichard <ryanmrichard@users.noreply.github.com>
2026-01-05 20:32:50 +0000

    deploy: 83a64fe8346af154e7e813ef3ba47d681459d9a3

--- wtf_cxx_api/search/functions_e.js
+++ wtf_cxx_api/search/functions_e.js
@@ -4,5 +4,7 @@ var searchData=
   ['visit_5fcontiguous_5fbuffer_1',['visit_contiguous_buffer',['../classwtf_1_1buffer_1_1FloatBuffer.html#a1a61270d41b7b3c12b74fa06c27a131a',1,'wtf::buffer::FloatBuffer']]],
   ['visit_5fcontiguous_5fbuffer_5fview_2',['visit_contiguous_buffer_view',['../classwtf_1_1buffer_1_1BufferView.html#a742e865aabb928a6da1e634a2a069a03',1,'wtf::buffer::BufferView']]],
   ['visit_5fcontiguous_5fmodel_3',['visit_contiguous_model',['../classwtf_1_1buffer_1_1detail___1_1ContiguousModel.html#afe35d8cdbb7eb8be7ac1978657096ed0',1,'wtf::buffer::detail_::ContiguousModel']]],
-  ['visit_5fcontiguous_5fview_5fmodel_4',['visit_contiguous_view_model',['../classwtf_1_1buffer_1_1detail___1_1ContiguousViewModel.html#a1391ab40cdb6d5548adc4c1e12693aee',1,'wtf::buffer::detail_::ContiguousViewModel']]]
+  ['visit_5fcontiguous_5fview_5fmodel_4',['visit_contiguous_view_model',['../classwtf_1_1buffer_1_1detail___1_1ContiguousViewModel.html#a1391ab40cdb6d5548adc4c1e12693aee',1,'wtf::buffer::detail_::ContiguousViewModel']]],
+  ['visit_5ffloat_5',['visit_float',['../classwtf_1_1fp_1_1Float.html#a33e7f72c4f869b64b88db4c20c440f4d',1,'wtf::fp::Float']]],
+  ['visit_5ffloat_5fmodel_6',['visit_float_model',['../classwtf_1_1fp_1_1detail___1_1FloatModel.html#acb5d8e150f1964f3323eeaa659d895a5',1,'wtf::fp::detail_::FloatModel']]]
 ];
