df65c392ffc7fd3edf4691e908126dcb3e862f34
ryanmrichard <ryanmrichard@users.noreply.github.com>
2026-01-06 21:48:58 +0000

    deploy: 66d0873246e15af66fea47fa7313e8b7cc55011f

--- wtf_cxx_api/search/concepts_0.js
+++ wtf_cxx_api/search/concepts_0.js
@@ -7,8 +7,9 @@ var searchData=
   ['wtf_3a_3aconcepts_3a_3afpiterator_4',['FPIterator',['../conceptwtf_1_1concepts_1_1FPIterator.html',1,'wtf::concepts']]],
   ['wtf_3a_3aconcepts_3a_3aispointer_5',['IsPointer',['../conceptwtf_1_1concepts_1_1IsPointer.html',1,'wtf::concepts']]],
   ['wtf_3a_3aconcepts_3a_3aisreference_6',['IsReference',['../conceptwtf_1_1concepts_1_1IsReference.html',1,'wtf::concepts']]],
-  ['wtf_3a_3aconcepts_3a_3aunmodified_7',['Unmodified',['../conceptwtf_1_1concepts_1_1Unmodified.html',1,'wtf::concepts']]],
-  ['wtf_3a_3aconcepts_3a_3aunmodifiedfloatingpoint_8',['UnmodifiedFloatingPoint',['../conceptwtf_1_1concepts_1_1UnmodifiedFloatingPoint.html',1,'wtf::concepts']]],
-  ['wtf_3a_3aconcepts_3a_3avolatilequalified_9',['VolatileQualified',['../conceptwtf_1_1concepts_1_1VolatileQualified.html',1,'wtf::concepts']]],
-  ['wtf_3a_3aconcepts_3a_3awtffloat_10',['WTFFloat',['../conceptwtf_1_1concepts_1_1WTFFloat.html',1,'wtf::concepts']]]
+  ['wtf_3a_3aconcepts_3a_3astreaminsertable_7',['StreamInsertable',['../conceptwtf_1_1concepts_1_1StreamInsertable.html',1,'wtf::concepts']]],
+  ['wtf_3a_3aconcepts_3a_3aunmodified_8',['Unmodified',['../conceptwtf_1_1concepts_1_1Unmodified.html',1,'wtf::concepts']]],
+  ['wtf_3a_3aconcepts_3a_3aunmodifiedfloatingpoint_9',['UnmodifiedFloatingPoint',['../conceptwtf_1_1concepts_1_1UnmodifiedFloatingPoint.html',1,'wtf::concepts']]],
+  ['wtf_3a_3aconcepts_3a_3avolatilequalified_10',['VolatileQualified',['../conceptwtf_1_1concepts_1_1VolatileQualified.html',1,'wtf::concepts']]],
+  ['wtf_3a_3aconcepts_3a_3awtffloat_11',['WTFFloat',['../conceptwtf_1_1concepts_1_1WTFFloat.html',1,'wtf::concepts']]]
 ];
